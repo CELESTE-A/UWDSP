13b983c90f701ad5461e216f502379a8d52e104e
zhanglingxiang <zhanglingxiang@hrbeu.edu.cn>
2026-01-02 13:36:26 +0800

    Add core library implementation

---
+++ src/uwasim_core.cpp
@@ -0,0 +1,9 @@
+﻿#include "uwasim/uwasim_core.hpp"
+
+namespace uwasim {
+
+std::string banner() {
+  return "UwaSimCpp: underwater acoustic signal-level simulation (C++20)";
+}
+
+}  // namespace uwasim
